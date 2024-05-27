5ca9bb87a99de16916b7d8c62abab4152536a9a3
Ollie Henry <ollie@noidea.noidea>
2024-05-27 23:57:12 +0100

    Complete exercise 1.1

---
+++ 1-binomial-pricer/Main01.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //display message
+    cout << "Hi there" << endl;
+
+    //pause prram
+    char x; cin >> x;
+
+    return 0;
+}
---
+++ 1-binomial-pricer/notes.md
@@ -0,0 +1,5 @@
+To do:
+
+- Verify that the binomial model admits no arbitrage when D < R < U
+- Derive the CRR procedure
+- Interpret the risk-neutral probability
